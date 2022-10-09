e4e372edf7f230e743b5c7406881ad56c7d0b289
DavisSnow002 <nieveshernandezjesusdavid@gmail.com>
2022-10-09 18:01:28 -0500

    tests

--- tests.py
+++ tests.py
@@ -23,26 +23,7 @@ class TesisFactory(AbsDocumentFact):
 
     def crearDocumento(self, Anno, Autores, Edicion, format, Idiomas, Isbn,Paginas,Titulo):
         return Tesis(Anno, Autores, Edicion, format, Idiomas, Isbn,Paginas,Titulo)
-class testFactories(unittest.TestCase):
 
-    def test_revista_factory(self):
-        magazine_one = Cliente(RevistaFactory())
-        self.assertEqual(magazine_one.__str__(), "nombre: 2001, edicion: LAPORTE, titulo: Fundamento Algoritmo,")
-        self.assertTrue(isinstance(magazine_one, Revista))
-    def test_tesis_factory(self):
-        tesis = Cliente(TesisFactory())
-        self.assertEqual(tesis.__str__(), "nombre: 2001, edicion: LAPORTE, titulo: Fundamento Algoritmo,")
-        self.assertTrue(isinstance(tesis, Tesis))
-    def test_cientifico_factory(self):
-        cientf = Cliente(CientificoFactory())
-        self.assertEqual(cientf.__str__(), "nombre: 2001, edicion: LAPORTE, titulo: Fundamento Algoritmo,")
-        self.assertTrue(isinstance(cientf, Cientifico))
-    def test_libro_factory(self):
-        libro = Cliente(LibroFactory())
-        self.assertEqual(libro.__str__(), "nombre: 2001, edicion: LAPORTE, titulo: Fundamento Algoritmo,")
-        self.assertTrue(isinstance(libro, Libro))
-if __name__ == "__main__":
-    unittest.main()   
 
 class TestDocumentos(unittest.TestCase):
     datos_1 = {'Anno': 2001,
@@ -72,6 +53,24 @@ class TestDocumentos(unittest.TestCase):
         self.assertEqual(book_one.getCategoria(), category.INGENIERIA)
 if __name__ == "__main__":
     unittest.main()
-    
- 
+class testFactories(unittest.TestCase):
+
+    def test_revista_factory(self):
+        magazine_one = Cliente(RevistaFactory())
+        self.assertEqual(magazine_one.__str__(), "nombre: 2001, edicion: LAPORTE, titulo: Fundamento Algoritmo,")
+        self.assertTrue(isinstance(magazine_one, Revista))
+    def test_tesis_factory(self):
+        tesis = Cliente(TesisFactory())
+        self.assertEqual(tesis.__str__(), "nombre: 2001, edicion: LAPORTE, titulo: Fundamento Algoritmo,")
+        self.assertTrue(isinstance(tesis, Tesis))
+    def test_cientifico_factory(self):
+        cientf = Cliente(CientificoFactory())
+        self.assertEqual(cientf.__str__(), "nombre: 2001, edicion: LAPORTE, titulo: Fundamento Algoritmo,")
+        self.assertTrue(isinstance(cientf, Cientifico))
+    def test_libro_factory(self):
+        libro = Cliente(LibroFactory())
+        self.assertEqual(libro.__str__(), "nombre: 2001, edicion: LAPORTE, titulo: Fundamento Algoritmo,")
+        self.assertTrue(isinstance(libro, Libro))
+if __name__ == "__main__":
+    unittest.main()   
 
